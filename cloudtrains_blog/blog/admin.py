d3473069f55a3faed570d4eef2aa00c24214032e
sudeep0901 <sudeep.tech.patel@gmail.com>
2020-01-03 17:04:23 +0530

    django-admin interface cutomized, ordering added to admin.py

--- cloudtrains_blog/blog/admin.py
+++ cloudtrains_blog/blog/admin.py
@@ -14,3 +14,4 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
