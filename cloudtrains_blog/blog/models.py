2f6b0da308304f8a248108f5c5fdfb1505c66f19
sudeep0901 <sudeep.tech.patel@gmail.com>
2020-01-03 16:44:59 +0530

    blog model created - Moded datetime issue  fixed + Post Model migration finished

--- cloudtrains_blog/blog/models.py
+++ cloudtrains_blog/blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now())
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
