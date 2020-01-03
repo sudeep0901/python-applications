b75fc32128d8e4b0aca9bcb54bfb7faffbc9f635
sudeep0901 <sudeep.tech.patel@gmail.com>
2020-01-03 16:42:12 +0530

    blog model created - Moded datetime issue  fixed + Post Model migration finished

--- README.md
+++ README.md
@@ -19,3 +19,12 @@ Running server with dev setting
 python manage.py runserver 127.0.0.1:8001 --settings=cloudtrains_blog.settings
 
 python manage.py startapp blog
+
+
+python manage.py makemigrations blog
+
+Check the SQL which django to apply when migrating schema to database
+
+python manage.py sqlmigrate blog 0001
+
+python manage.py migrate
--- cloudtrains_blog/blog/models.py
+++ cloudtrains_blog/blog/models.py
@@ -1,5 +1,4 @@
-from datetime import timezone
-
+from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -16,7 +15,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now())
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
@@ -24,8 +23,9 @@ class Post(models.Model):
                               default='draft')
     deleted = models.BooleanField(default=False)
     delete_on = models.DateTimeField(null=True, blank=True)
+
     class Meta:
-        ordering = '-publish'
+        ordering = ['-publish']
 
     def __str__(self):
         return self.title
