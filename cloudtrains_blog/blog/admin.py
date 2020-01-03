88db62959b447e4171fa7bc64a69e7ab7cc7b87d
sudeep0901 <sudeep.tech.patel@gmail.com>
2020-01-03 17:03:35 +0530

    django-admin interface cutomized, filter, list display etc. added to admin.py

--- cloudtrains_blog/blog/admin.py
+++ cloudtrains_blog/blog/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'deleted', 'publish', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
